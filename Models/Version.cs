75e0afee537fe3f91c58f5c3ae0823f304f27a60
tasos.kleisas <tasos.kleisas@gmail.com>
2025-11-21 10:31:40 +0200

    Fix for commit hash

--- Models/Version.cs
+++ Models/Version.cs
@@ -1,18 +1,12 @@
 namespace NoPasaranFC.Models
 {
-    public static class Version
+    public static partial class Version
     {
         public const string MAJOR = "1";
         public const string MINOR = "0";
         public const string PATCH = "5";
         
-        // This will be set at compile time by MSBuild
-        private const string COMMIT_HASH = 
-#if GITCOMMITHASH
-            $"{GITCOMMITHASH}";
-#else
-            "unknown";
-#endif
+        // COMMIT_HASH is defined in the auto-generated GitCommitHash.cs file
         
         public static string GetVersion()
         {
@@ -26,9 +20,9 @@ namespace NoPasaranFC.Models
         
         public static string GetFullVersion()
         {
-            if (COMMIT_HASH == "unknown")
-                return $"v{GetVersion()}";
-            return $"v{GetVersion()}-{COMMIT_HASH}";
+            return COMMIT_HASH == "unknown" 
+                ? $"v{GetVersion()}" 
+                : $"v{GetVersion()}-{COMMIT_HASH}";
         }
     }
 }
